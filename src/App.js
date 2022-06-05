5e886f7bdfeca75ad4b057acb08d80984bf2c2df
pedrocz82 <pedrocz@hotmail.com>
2022-06-05 18:15:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [primeiroNumero, setPrimeiroNumero] = useState(0);
+  const [operacao, setOperacao] = useState('');
+  const [segundoNumero, setSegundoNumero] = useState(0);
+  const [resultado, setResultado] = useState(0);
+
+  function handleSubmit(e){
+    e.preventDefault();
+  
+
+  switch (operacao) {
+    case '+':
+      setResultado(primeiroNumero+segundoNumero);
+      break;
+
+      case '-':
+      setResultado(primeiroNumero-segundoNumero);
+      break;
+
+      case '/':
+      setResultado(primeiroNumero/segundoNumero);
+      break;
+
+      case '*':
+      setResultado(primeiroNumero*segundoNumero);
+      break;
+
+    default:
+      break
+  }
+}
+
+return (
+
+  <>
+    <form onSubmit={handleSubmit}>
+      <input type="number" onChange={(e) => { setPrimeiroNumero(e.target.valueAsNumber) }} value={primeiroNumero} />
+      <select onChange={(e) => { setOperacao(e.target.value) }} value={operacao}>
+        <option value="">--Selecione a operação--</option>
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="/">/</option>
+        <option value="*">*</option>
+      </select>
+
+      <input type="number" onChange={(e) => { setSegundoNumero(e.target.valueAsNumber) }} value={segundoNumero} />
+      <button type="submit">CALCULAR</button><b>RESULTADO: </b>{resultado}
+    </form>
+
+
+  </>
+
+
+);
 }
 
 export default App;
